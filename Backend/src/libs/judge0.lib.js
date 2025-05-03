7ecc576f124e08623841ee8df46e627d94ba19c1
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-05-03 18:30:39 +0530

    feat: add Judge0 library for language mapping and batch submission

---
+++ Backend/src/libs/judge0.lib.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const getJudge0LanguageId = (language) => {
+    const languageMap = {
+        "PYTHON": 71,
+        "JAVA": 62,
+        "JAVASCRIPT": 63,
+    };
+
+    return languageMap[language.toUpperCase()];
+}
+
+const submitBatch = async (submissions) => {
+    const {data} = await axios.post(`${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`, {
+        submissions
+    })
+
+    console.log("Submitted batch: ", data);
+
+    return data;
+};
+
+const sleep = (seconds) => new Promise((resolve) => setTimeout(resolve, seconds*1000));
+
+const pollBatchResults = async (tokens) => {
+    while(true) {
+        const {data} = await axios.get(`${process.env.JUDGE0_API_URL}/submissions/batch`, {
+            params: {
+                tokens: tokens.join(","),
+                base64_encoded: false,
+            }
+        });
+
+        const results = data.submissions;
+
+        const isAllDone = results.every(
+            (result) => result.status.id >= 3
+        );
+
+        if(isAllDone) return results
+
+        await sleep(1);
+    }
+}
+
+export {
+    getJudge0LanguageId,
+    submitBatch,
+    pollBatchResults
+}
