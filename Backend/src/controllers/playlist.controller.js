46b6ac9dfe0270b3591d0fce7a47888e3798be0e
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-05-11 09:01:16 +0530

    feat: playlist

---
+++ Backend/src/controllers/playlist.controller.js
@@ -0,0 +1,19 @@
+import { db } from "../libs/db.js";
+
+const getAllPlaylists = async (req, res) => {};
+
+const getPlaylistById = async (req, res) => {};
+
+const createPlaylist = async (req, res) => {};
+
+const updatePlaylistById = async (req, res) => {};
+
+const deletePlaylistById = async (req, res) => {};
+
+export {
+    getAllPlaylists,
+    getPlaylistById,
+    createPlaylist,
+    updatePlaylistById,
+    deletePlaylistById
+}
---
+++ Backend/src/routes/playlist.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { authMiddleware } from '../middleware/auth.middleware.js';
+import { createPlaylist, deletePlaylistById, getAllPlaylists, getPlaylistById, updatePlaylistById } from '../controllers/playlist.controller.js';
+
+const playlistRoutes = express.Router();
+
+playlistRoutes.get("/get-all-playlists", authMiddleware, getAllPlaylists);
+playlistRoutes.get("/get-playlist/:playlistId", authMiddleware, getPlaylistById);
+playlistRoutes.post("/create-playlist", authMiddleware, createPlaylist);
+playlistRoutes.put("/update-playlist/:playlistId", authMiddleware, updatePlaylistById);
+playlistRoutes.delete("/delete-playlist/:playlistId", authMiddleware, deletePlaylistById);
+
+export default playlistRoutes;
