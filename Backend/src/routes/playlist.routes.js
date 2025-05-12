f168ce30d12a96939faa8af0982f2961a7935408
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-05-12 10:01:00 +0530

    feat(playlist): add route to add problem to playlist

--- Backend/src/routes/playlist.routes.js
+++ Backend/src/routes/playlist.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/auth.middleware.js';
-import { createPlaylist, deletePlaylistById, deleteProblemFromPlaylist, getAllPlaylists, getPlaylistById, updatePlaylistById } from '../controllers/playlist.controller.js';
+import { addProblemToPlaylist, createPlaylist, deletePlaylistById, deleteProblemFromPlaylist, getAllPlaylists, getPlaylistById, updatePlaylistById } from '../controllers/playlist.controller.js';
 
 const playlistRoutes = express.Router();
 
@@ -8,6 +8,7 @@ playlistRoutes.get("/get-all-playlists", authMiddleware, getAllPlaylists);
 playlistRoutes.get("/get-playlist/:playlistId", authMiddleware, getPlaylistById);
 playlistRoutes.post("/create-playlist", authMiddleware, createPlaylist);
 playlistRoutes.put("/update-playlist/:playlistId", authMiddleware, updatePlaylistById);
+playlistRoutes.post("/:playlistId/add-problem/:problemId", authMiddleware, addProblemToPlaylist);
 playlistRoutes.delete("/delete-playlist/:playlistId", authMiddleware, deletePlaylistById);
 playlistRoutes.delete("/:playlistId/delete-problem/:problemId", authMiddleware, deleteProblemFromPlaylist);
 
