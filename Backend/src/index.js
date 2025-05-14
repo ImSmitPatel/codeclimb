e9e336c858f9d65f2742a6de3138531260801273
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-05-14 11:07:36 +0530

    chore: setup Swagger UI and add basic rate limiting

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 
 import authRoutes from "./routes/auth.routes.js";
 import problemRoutes from "./routes/problem.routes.js";
@@ -11,9 +14,33 @@ import submissionRoutes from "./routes/submission.routes.js";
 dotenv.config();
 
 const app = express();
+const limiterConfig = rateLimit({
+    windowMs: 30 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+    message: "Too many requests from this IP, please try again after 30 minutes"
+})
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Codeclimb API',
+            version: '1.0.0',
+            description: 'API documentation for Codeclimb',
+        },
+        servers:[
+            {
+                url: 'http://localhost:8080'
+            },
+        ],
+    },
+    apis: ["./src/routes/*.js"],
+};
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(limiterConfig);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req, res) => {
     res.send("Hello Welcome to Codeclimb©️🔥");
