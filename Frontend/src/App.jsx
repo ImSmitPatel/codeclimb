4e16f5234b8fac35746090f405cc1c587d693f10
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-05-20 21:54:27 +0530

    feat (frontend): login + signup page ready.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import {Routes, Route, Navigate} from "react-router-dom"
+import HomePage from "./page/HomePage"
+import LoginPage from "./page/LoginPage"
+import SignUpPage from "./page/SignUpPage"
 
 const App = () => {
+  
+  let authUser = false;
+
   return (
-    <div className='flex flex-col align-baseline'>
-      App
+    <div className='flex flex-col items-center justify-start'>
+      <Routes>
+        <Route
+        path='/'
+        element={authUser ? <HomePage/> : <LoginPage/>}
+        />
+
+        <Route
+        path='/login'
+        element={!authUser ? <LoginPage/> : <Navigate to={"/"}/>}
+        />
+
+        <Route
+          path='/signup'
+          element={!authUser ? <SignUpPage/> : <Navigate to={"/"}/>}
+        />
+      </Routes>
     </div>
   )
 }
