94ecf08df38f37fc38c1a488a50989168a3604f5
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2025-10-25 10:36:48 +0530

    chore(ui): add authUser console log and update fallback avatar URL

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -8,16 +8,18 @@ import LogoutButton from './LogoutButton'
 const Navbar = () => {
     const { authUser } = useAuthStore()
 
+    console.log("authUser", authUser)
+
 
     return (
         <nav className="sticky top-0 z-50 w-full py-5">
             <div className="flex w-full justify-between mx-auto max-w-4xl bg-black/15 shadow-lg shadow-neutral-600/5 backdrop-blur-lg border border-gray-200/10 p-4 rounded-2xl">
                 {/* Logo Section */}
                 <Link to="/" className="flex items-center gap-3 cursor-pointer">
-                {/* https://i.ibb.co/pj9BQ6MB/codeclimb-no-bg.png */}
+                    {/* https://i.ibb.co/pj9BQ6MB/codeclimb-no-bg.png */}
                     <img src="https://i.ibb.co/S40hFdR6/codeclimb-logo-only-no-bg.png" className="h-25 w-25 text-primary border-none px-2 py-2" />
                     <span className="text-lg md:text-2xl font-bold tracking-tight text-white hidden md:block">
-                        CodeClimb 
+                        CodeClimb
                     </span>
                 </Link>
 
@@ -25,58 +27,58 @@ const Navbar = () => {
                 <div className="flex items-center gap-8">
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle avatar flex flex-row ">
-                        <div className="w-10 rounded-full ">
-                            <img
-                            src={
-                                authUser?.image ||
-                                "../assets/codeclimb.png"
-                            }
-                            alt="User Avatar"
-                            className="object-cover"
-                            />
-                        </div>
-                    
+                            <div className="w-10 rounded-full ">
+                                <img
+                                    src={
+                                        authUser?.image ||
+                                        "https://avatar.iran.liara.run/public"
+                                    }
+                                    alt="User Avatar"
+                                    className="object-cover"
+                                />
+                            </div>
+
                         </label>
                         <ul
                             tabIndex={0}
                             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 space-y-3"
                         >
-                        {/* Admin Option */}
-                        
+                            {/* Admin Option */}
 
-                        {/* Common Options */}
-                        <li>
-                            <p className="text-base font-semibold">
-                                Hello, {authUser?.name}
-                            </p>
-                            <hr className="border-gray-200/10" />
-                        </li>
-                        <li>
-                            <Link
-                            to="/profile"
-                            className="hover:bg-primary hover:text-white text-base font-semibold"
-                            >
-                            <User className="w-4 h-4 mr-2" />
-                            My Profile
-                            </Link>
-                        </li>
-                        {authUser?.role === "ADMIN" && (
+
+                            {/* Common Options */}
                             <li>
-                            <Link
-                                to="/add-problem"
-                                className="hover:bg-primary hover:text-white text-base font-semibold"
-                            >
-                                <Code className="w-4 h-4 mr-1" />
-                                Add Problem
-                            </Link>
+                                <p className="text-base font-semibold">
+                                    Hello, {authUser?.name}
+                                </p>
+                                <hr className="border-gray-200/10" />
                             </li>
-                        )}
-                        <li>
-                            <LogoutButton className="hover:bg-primary hover:text-white">
-                                <LogOut className="w-4 h-4 mr-2" />
-                                Logout
-                            </LogoutButton>
-                        </li>
+                            <li>
+                                <Link
+                                    to="/profile"
+                                    className="hover:bg-primary hover:text-white text-base font-semibold"
+                                >
+                                    <User className="w-4 h-4 mr-2" />
+                                    My Profile
+                                </Link>
+                            </li>
+                            {authUser?.role === "ADMIN" && (
+                                <li>
+                                    <Link
+                                        to="/add-problem"
+                                        className="hover:bg-primary hover:text-white text-base font-semibold"
+                                    >
+                                        <Code className="w-4 h-4 mr-1" />
+                                        Add Problem
+                                    </Link>
+                                </li>
+                            )}
+                            <li>
+                                <LogoutButton className="hover:bg-primary hover:text-white">
+                                    <LogOut className="w-4 h-4 mr-2" />
+                                    Logout
+                                </LogoutButton>
+                            </li>
                         </ul>
                     </div>
                 </div>
