32e15d9435ff17378c8e1703b2a2bf482ef1fa0d
Smit Patel <87602550+ImSmitPatel@users.noreply.github.com>
2026-02-25 22:57:02 +0530

    production changes

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE == "production" ? "https://api.codeclimb.in/api/v1" : "http://localhost:8080/api/v1",
-    withCredentials: true
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
